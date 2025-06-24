97c9860a8086566c2d2bbabe9c894c1dbd68417e
uqhmcdou_local <uqhmcdou_local@SMP-F3NGMX3.uq.edu.au>
2025-06-24 23:10:37 +1000

    removes clustering from init

--- litmus/__init__.py
+++ litmus/__init__.py
@@ -6,7 +6,6 @@ __license__     = "Free to use"
 __description__ = "JAX-based lag recovery program for AGN reverberation mapping"
 
 
-
 import litmus._utils
 import litmus._types
 import litmus.lin_scatter
@@ -15,7 +14,7 @@ import litmus.gp_working
 import litmus.models
 import litmus.ICCF_working
 import litmus.fitting_methods
-import litmus._ss.clustering
+#import litmus._ss.clustering
 import litmus.mocks
 import litmus.logging
 
